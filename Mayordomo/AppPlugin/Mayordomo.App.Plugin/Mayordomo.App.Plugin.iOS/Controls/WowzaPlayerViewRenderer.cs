9f50ab37440e395c7ba18d5ef9f8b00cb46db66e
Carlos Diaz Lopez <ryankar90@hotmail.com>
2020-03-26 10:35:05 -0600

    add plugin, wowza

---
+++ Mayordomo/AppPlugin/Mayordomo.App.Plugin/Mayordomo.App.Plugin.iOS/Controls/WowzaPlayerViewRenderer.cs
@@ -0,0 +1,28 @@
+﻿using Mayordomo.App.Plugin.Controls;
+using Mayordomo.App.Plugin.iOS.Controls;
+using Plugin.WowzaClient;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(WowzaPlayerView), typeof(WowzaPlayerViewRenderer))]
+namespace Mayordomo.App.Plugin.iOS.Controls
+{
+    public class WowzaPlayerViewRenderer : ViewRenderer<WowzaPlayerView, UIView>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<WowzaPlayerView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+            {
+                SetNativeControl(new UIView());
+            }
+
+            if (e.NewElement != null)
+            {
+                WowzaClientManager.AttachPlayerView(Control);
+            }
+        }
+    }
+}
---
+++ Mayordomo/AppPlugin/Mayordomo.App.Plugin/Mayordomo.App.Plugin/Helpers/WowzaCloudConfig.cs
@@ -0,0 +1,23 @@
+﻿using Plugin.WowzaClient;
+
+namespace Mayordomo.App.Plugin.Helpers
+{
+    public class WowzaCloudConfig : IWowzaConfig
+    {
+        public WowzaCloudConfig()
+        {
+        }
+
+        public string HostAddress { get; set; }//=> "0467fc.entrypoint.cloud.wowza.com";
+
+        public int PortNumber { get; set; } //=> 1935;
+
+        public string ApplicationName { get; set; }// => "app-1264";
+
+        public string StreamName { get; set; } //=> "0e68bed6";
+
+        public string Username { get; set; } //=> "client39318";
+
+        public string Password { get; set; } //=> "password";
+    }
+}
