d39bcd0db76ef23cfd3e1f1b9147ae1b31bd4e76
Carlos Diaz Lopez <ryankar90@hotmail.com>
2020-12-22 21:20:33 -0600

    cre

---
+++ Mayordomo/MayordomoApi/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using MayordomoApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MayordomoApi.Controllers
+{
+    //[Authorize]
+    //[RoutePrefix("api/customers")]
+
+
+    [AllowAnonymous]
+    [RoutePrefix("api/login")]
+    public class LoginController : ApiController
+    {
+        [HttpGet]
+        [Route("validateemail")]
+        public async Task<IHttpActionResult> ValidateEmail()
+        {
+            return Ok(true);
+        }
+
+        //[HttpPost]
+        //[Route("authenticate")]
+        //public IHttpActionResult Authenticate(LoginRequest login)
+        //{
+            //if (login == null)
+              //  return Unauthorized();
+
+            //TODO: Validate credentials Correctly, this code is only for demo !!
+            //bool isCredentialValid = (login.Password == "123456");
+            //if (isCredentialValid)
+            //{
+               // var token = TokenGenerator.GenerateTokenJwt(login.Username);
+              //  return Ok(token);
+            //}
+            //else
+            //{
+             //   return Unauthorized();
+           // }
+        //}
+    }
+}
---
+++ Mayordomo/MayordomoApi/Entities/TokenResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MayordomoApi.Entities
+{
+    public class TokenResponse
+    {
+        public string Token { get; set; }
+        public DateTime Expired { get; set; }
+    }
+}
